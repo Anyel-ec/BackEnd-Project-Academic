8ebba1e0f8f0c1c820790080c245387d441d5c9d
anyel-ec <paulangel826@gmail.com>
2024-07-05 21:58:38 -0500

    start project in spring boot 3.3.1

---
+++ src/main/java/pe/edu/unamba/academic/ProjectPeruAcademicApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unamba.academic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPeruAcademicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPeruAcademicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/unamba/academic/ProjectPeruAcademicApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unamba.academic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPeruAcademicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
