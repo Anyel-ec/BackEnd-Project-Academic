a7f1add066aaf80032750210140a27fe49ce49df
JerickTwO <jerickjjtm774@gmail.com>
2025-01-04 23:29:00 -0500

    feat: refactoring security and login in all project

--- src/main/java/pe/edu/unamba/academic/services/UserService.java
+++ src/main/java/pe/edu/unamba/academic/services/UserService.java
@@ -11,7 +11,7 @@ import pe.edu.unamba.academic.models.UserInfo;
 import pe.edu.unamba.academic.repositories.UserRepository;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class UserService {
